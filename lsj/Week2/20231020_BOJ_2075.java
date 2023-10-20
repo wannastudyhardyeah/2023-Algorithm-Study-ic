807e8e212a9f59164f9849f2bfa3062aab28c65e
lsj8367 <lsj8367@naver.com>
2023-10-20 17:08:01 +0900

    feat: add boj 2075

---
+++ lsj/Week2/20231020_BOJ_2075.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                queue.offer(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            queue.poll();
+        }
+
+        System.out.println(queue.poll());
+    }
+}
